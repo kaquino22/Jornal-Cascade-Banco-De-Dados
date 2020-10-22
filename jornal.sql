238e9ed9be2471167e5d44701fa8603de85ee418
Kauã Aquino <kaquino@tecnologiaunica.local>
2020-10-22 11:16:21 -0300

    Subindo

---
+++ jornal.sql
@@ -0,0 +1,107 @@
+CREATE DATABASE jornal;
+USE jornal;
+
+CREATE TABLE categoria (
+IdCategoria int PRIMARY KEY auto_increment,
+Categoria varchar(255)
+);
+
+CREATE TABLE turma (
+IdTurma int PRIMARY KEY auto_increment,
+Turma varchar(5)
+);
+
+CREATE TABLE dia (
+Dia int PRIMARY KEY
+);
+
+CREATE TABLE mes (
+Mes int PRIMARY KEY
+);
+
+CREATE TABLE ano (
+Ano int PRIMARY KEY
+);
+
+CREATE TABLE redator (
+IdRedator int PRIMARY KEY auto_increment,
+Nome varchar(255),
+Sobrenome varchar(255),
+Email varchar(50),
+ddd int,
+Telefone varchar(15),
+Cidade varchar(255),
+IdTurma int,
+Dia int,
+Mes int,
+Ano int,
+
+FOREIGN KEY(IdTurma) REFERENCES turma (IdTurma) ON DELETE CASCADE ON UPDATE CASCADE,
+FOREIGN KEY(Dia) REFERENCES dia (Dia) ON DELETE CASCADE ON UPDATE CASCADE,
+FOREIGN KEY(Mes) REFERENCES mes (Mes) ON DELETE CASCADE ON UPDATE CASCADE,
+FOREIGN KEY(Ano) REFERENCES ano (Ano) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+CREATE TABLE noticia (
+IdNoticia int PRIMARY KEY auto_increment,
+Noticia text(10000),
+Titulo varchar(255),
+IdRedator int,
+Idcategoria int,
+Dia int,
+Mes int,
+Ano int,
+
+FOREIGN KEY(IdRedator) REFERENCES redator (IdRedator) ON DELETE CASCADE ON UPDATE CASCADE,
+FOREIGN KEY(Idcategoria) REFERENCES categoria (IdCategoria) ON DELETE CASCADE ON UPDATE CASCADE,
+FOREIGN KEY(Dia) REFERENCES dia (Dia) ON DELETE CASCADE ON UPDATE CASCADE,
+FOREIGN KEY(Mes) REFERENCES mes (Mes) ON DELETE CASCADE ON UPDATE CASCADE,
+FOREIGN KEY(Ano) REFERENCES ano (Ano) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+INSERT INTO `categoria`(`Categoria`) VALUES 
+('Ciências'),
+('Educação'),
+('Ecônomia'),
+('Saúde'),
+('Tecnologia'),
+('Sociologia'),
+('Filosofia'),
+('História'),
+('Politica');
+
+INSERT INTO `dia` VALUES 
+(1), (2), (3), (4), (5), (6), (7), (8), (9), (10),
+(11), (12), (13), (14), (15), (16), (17), (18), (19), (20),
+(21), (22), (23), (24), (25), (26), (27), (28), (29), (30),
+(31);
+
+INSERT INTO `mes` VALUES 
+(1), (2), (3), (4), (5), (6), (7), (8), (9), (10), (11), (12);
+
+INSERT INTO `ano` VALUES 
+(1950), (1951), (1952), (1953), (1954), (1955), (1956), (1957), (1958), (1959),
+(1960), (1961), (1962), (1963), (1964), (1965), (1966), (1967), (1968), (1969),
+(1970), (1971), (1972), (1973), (1974), (1975), (1976), (1977), (1978), (1979),
+(1980), (1981), (1982), (1983), (1984), (1985), (1986), (1987), (1988), (1989),
+(1990), (1991), (1992), (1993), (1994), (1995), (1996), (1997), (1998), (1999),
+(2000), (2001), (2002), (2003), (2004), (2005), (2006), (2007), (2008), (2009),
+(2010), (2011), (2012), (2013), (2014), (2015), (2016), (2017), (2018), (2019),
+(2020), (2021), (2022), (2023), (2024), (2025), (2026), (2027), (2028), (2029),
+(2030);
+
+INSERT INTO `turma`(`Turma`) VALUES 
+('1C'),
+('2C'),
+('3C'),
+('1A'),
+('2A'),
+('3A'),
+('1B'),
+('2B'),
+('3B'),
+('1F'),
+('2F'),
+('3F');
+
